9f756896b82b49dcb9409ee0e8db89c6fd23a4a8
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2023-01-02 06:10:54 -0800

    Added methods to get a regionDict and list of NGAs

--- pyseshat/pyseshat.py
+++ pyseshat/pyseshat.py
@@ -1,7 +1,33 @@
 import pandas as pd
 import pkg_resources
 import os
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import copy
 
+def tailoredSvd(data):
+    """A util method to call numpy to do an SVD after removing the mean
+    """
+    data -= np.mean(data, axis=0)
+    P, D, Q = np.linalg.svd(data, full_matrices=False)
+    return P, D, Q
+
+def loadPNAS2017CC(scale=False):
+    """Load the CC data from the 2017 PNAS article. Both the array and CC names
+    are returned.
+
+    Keyword arguments:
+    scale -- Whether to scale the CC array (default False, do not scale)
+    """
+    CC_df = loadSeshatDataset(version='PNAS2017', flavor='Imputations')
+    CC_names = ['PolPop','PolTerr', 'CapPop',
+                'levels', 'government','infrastr',
+                'writing', 'texts', 'money']
+    CC_array = CC_df.loc[:, CC_names].values
+    if scale:
+        CC_array = StandardScaler().fit_transform(CC_array)
+    return CC_array, CC_names
+ 
 def loadSeshatDataset(version, flavor=None):
     """Load a seshat dataset
 
@@ -46,7 +72,6 @@ def loadSeshatDataset(version, flavor=None):
         file_path = os.path.join(data_dir, file_name)
         return pd.read_csv(file_path)
 
-
 def getEquinoxWorksheets():
     """A utility function for getting the worksheets in the Equinox Excel file
     """
@@ -64,3 +89,121 @@ def getEquinoxWorksheets():
             'Scale_MI',
             'Class_MI']
  
+def getRegionDict(version):
+    """Get a dictionary where the key is the region (e.g., 'Africa') and the
+    entries are lists of NGAs
+
+    Keyword arguments:
+    version -- Which version is this for? e.g., PNAS2017 or Equinox
+    """
+
+    # There are any number of ways this method could be implemented. I have
+    # chosen to simply create the full regionDict directly for each case.
+    regionDict = dict()
+
+    if version == 'Equinox':
+        regionDict["Africa"] =\
+            ["Ghanaian Coast",
+             "Niger Inland Delta",
+             "Upper Egypt"]
+        regionDict["Europe"] =\
+            ["Iceland",
+             "Crete", # new
+             "Latium",
+             "Paris Basin"]
+        regionDict["Central Eurasia"] =\
+            ["Lena River Valley",
+             "Orkhon Valley",
+             "Sogdiana"]
+        regionDict["Southwest Asia"] =\
+            ["Galilee", # new
+             "Konya Plain",
+             "Southern Mesopotamia", # new
+             "Susiana",
+             "Yemeni Coastal Plain"]
+        regionDict["South Asia"] =\
+            ["Garo Hills",
+             "Deccan",
+             "Kachi Plain",
+             "Middle Ganga"] # new
+        regionDict["Southeast Asia"] =\
+            ["Cambodian Basin",
+             "Central Java",
+             "Kapuasi Basin"]
+        regionDict["East Asia"] =\
+            ["Kansai",
+             "Southern China Hills",
+             "Middle Yellow River Valley"]
+        regionDict["North America"] =\
+            ["Cahokia",
+             "Basin of Mexico", # new
+             "Finger Lakes",
+             "Valley of Oaxaca"]
+        regionDict["South America"] =\
+            ["Cuzco",
+             "Lowland Andes",
+             "North Colombia"]
+        regionDict["Oceania-Australia"] =\
+            ["Big Island Hawaii",
+             "Chuuk Islands",
+             "Oro PNG"]
+    elif version == 'PNAS2017':
+        regionDict["Africa"] =\
+            ["Ghanaian Coast",
+             "Niger Inland Delta",
+             "Upper Egypt"]
+        regionDict["Europe"] =\
+            ["Iceland",
+             "Latium",
+             "Paris Basin"]
+        regionDict["Central Eurasia"] =\
+            ["Lena River Valley",
+             "Orkhon Valley",
+             "Sogdiana"]
+        regionDict["Southwest Asia"] =\
+            ["Konya Plain",
+             "Susiana",
+             "Yemeni Coastal Plain"]
+        regionDict["South Asia"] =\
+            ["Garo Hills",
+             "Deccan",
+             "Kachi Plain"]
+        regionDict["Southeast Asia"] =\
+            ["Cambodian Basin",
+             "Central Java",
+             "Kapuasi Basin"]
+        regionDict["East Asia"] =\
+            ["Kansai",
+             "Southern China Hills",
+             "Middle Yellow River Valley"]
+        regionDict["North America"] =\
+            ["Cahokia",
+             "Finger Lakes",
+             "Valley of Oaxaca"]
+        regionDict["South America"] =\
+            ["Cuzco",
+             "Lowland Andes",
+             "North Colombia"]
+        regionDict["Oceania-Australia"] =\
+            ["Big Island Hawaii",
+             "Chuuk Islands",
+             "Oro PNG"]
+    else:
+        raise ValueError('Unrecognized version = ' + version)
+    return regionDict
+ 
+def getNGAs(version):
+    """Get a list of all NGAs.
+
+    Keyword arguments:
+    version -- Which version is this for? e.g., PNAS2017 or Equinox
+    """
+    # The core functionality is in getRegionDict
+    regionDict = getRegionDict(version)
+    NGAs = list()
+    for key in regionDict.keys():
+        for nga in regionDict[key]:
+            NGAs.append(nga)
+    
+    NGAs.sort()
+    return NGAs
--- tests/test_pyseshat.py
+++ tests/test_pyseshat.py
@@ -1,5 +1,6 @@
 import unittest
 from pyseshat.pyseshat import *
+import math
 
 class TestPyseshat(unittest.TestCase):
 
@@ -29,6 +30,66 @@ class TestPyseshat(unittest.TestCase):
         for flavor in ['Imputations', 'PCs']:
             df = loadSeshatDataset(version='PNAS2017', flavor=flavor)
             self.assertTrue(df.shape[0] > 0)
+
+    def test_loadPNAS2017CC(self):
+        # Check that we can load both the unscaled and scaled CC arrays
+        CC_array_unscaled, _ = loadPNAS2017CC()
+        self.assertEqual(CC_array_unscaled.shape, (8280, 9))
+        CC_array_scaled, _ = loadPNAS2017CC(scale=True)
+        self.assertEqual(CC_array_scaled.shape, (8280, 9))
+
+        # Iterate over columns to check the scaling
+        for cc_num in range(9):
+            CC_unscaled = CC_array_unscaled[:,cc_num]
+            self.assertNotAlmostEqual(np.mean(CC_unscaled), 0, places=8)
+            self.assertNotAlmostEqual(np.std(CC_unscaled), 1, places=8)
+            CC_scaled = CC_array_scaled[:,cc_num]
+            self.assertAlmostEqual(np.mean(CC_scaled), 0, places=8)
+            self.assertAlmostEqual(np.std(CC_scaled), 1, places=8)
+
+            CC_rescaled = CC_unscaled - np.mean(CC_unscaled)
+            CC_rescaled = CC_rescaled / np.std(CC_rescaled)
+            # TODO: there most be a better way to check equality for vectors
+            for i in range(len(CC_rescaled)):
+                self.assertAlmostEqual(CC_scaled[i], CC_rescaled[i], places=8)
+
+    def test_getRegionDict(self):
+        # Ensure an error is thrown for an unsupported version
+        with self.assertRaises(ValueError):
+            _ = getRegionDict(version='Bad Version')
+
+        # Check the regionDict for Equinox
+        regionDict = getRegionDict('Equinox')
+        self.assertEqual(len(regionDict), 10)
+        self.assertEqual(len(regionDict['Africa']), 3)
+        self.assertEqual(len(regionDict['Europe']), 4)
+        self.assertEqual(len(regionDict['Central Eurasia']), 3)
+        self.assertEqual(len(regionDict['Southwest Asia']), 5)
+        self.assertEqual(len(regionDict['South Asia']), 4)
+        self.assertEqual(len(regionDict['Southeast Asia']), 3)
+        self.assertEqual(len(regionDict['East Asia']), 3)
+        self.assertEqual(len(regionDict['North America']), 4)
+        self.assertEqual(len(regionDict['South America']), 3)
+        self.assertEqual(len(regionDict['Oceania-Australia']), 3)
+
+        # Check the regionDict for PNAS2017 
+        regionDict = getRegionDict('PNAS2017')
+        self.assertEqual(len(regionDict), 10)
+        for key in regionDict.keys():
+            self.assertEqual(len(regionDict[key]), 3)
+
+    def test_getNGAs(self):
+        # Ensure an error is thrown for an unsupported version
+        with self.assertRaises(ValueError):
+            _ = getNGAs(version='Bad Version')
+
+        # Check Equinox
+        NGAs = getNGAs(version='Equinox')
+        self.assertEqual(len(NGAs), 35)
+
+        # Check PNAS2017 
+        NGAs = getNGAs(version='PNAS2017')
+        self.assertEqual(len(NGAs), 30)
 
 if __name__ == '__main__':
     unittest.main()
